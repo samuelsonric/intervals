768cbb1a1e8e8e1ff258dfaad878cf80010dc740
Richard Samuelson <samuelson.rick@gmail.com>
2021-02-03 15:19:11 -0800

    import SignedMeasure, iter_approx, approx

--- intervals/__init__.py
+++ intervals/__init__.py
@@ -1,2 +1,3 @@
-from intervals.simple_function import IterSimpleFunctionAlgebra, SimpleFunction
+from intervals.simple_function import IterSimpleFunctionAlgebra, SimpleFunction, iter_approx, approx
 from intervals.intervals import Intervals
+from intervals.measure import SignedMeasure
