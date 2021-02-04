8d60b88c8b3ace37ec70d05c7ce18521e6db63b3
Richard Samuelson <samuelson.rick@gmail.com>
2021-02-04 12:32:53 -0800

    bugfix

--- intervals/plotting.py
+++ intervals/plotting.py
@@ -1,5 +1,6 @@
 from itertools import takewhile, dropwhile
 import matplotlib.pyplot as plt
+from itertools import cycle
 
 
 def get_bar_inputs(sfunc, start, stop):
@@ -7,16 +8,16 @@ def get_bar_inputs(sfunc, start, stop):
         return (i[0], i[1], i[2] - i[1])
 
     tfilt = lambda i: i[1] < stop
-    dfilt = lambda i: i[2] < start
+    dfilt = lambda i: i[2] <= start
 
     g = map(mapper, takewhile(tfilt, dropwhile(dfilt, sfunc.iter_triples())))
 
     height, x, width = map(list, zip(*g))
 
     x[0] = start
-    width[0] = width[1] - start
+    width[0] = x[1] - start
     width[-1] = stop - x[-1]
-
+    print(x, height, width)
     return (x, height, width)
 
 
@@ -24,12 +25,12 @@ def plot_sfuncs(
     *sfuncs,
     xlim=(-10, 10),
     ylim=None,
-    figsize=(8, 6),
+    figsize=(4, 3),
     colors=("b", "g", "r", "c", "m", "y", "k"),
 ):
     plt.figure(figsize=figsize)
     for x, c in zip(sfuncs, cycle(colors)):
-        plt.bar(*get_bar_inputs(x, *xlim), color=c)
+        plt.bar(*get_bar_inputs(x, *xlim), color=c, align="edge")
     plt.xlim(xlim)
     if ylim is not None:
         plt.ylim(ylim)
