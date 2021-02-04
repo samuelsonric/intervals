e5d1c64381e414c3cb5d0684c812addcd31d0bad
Richard Samuelson <samuelson.rick@gmail.com>
2021-02-04 14:28:21 -0800

    redid previous commit

--- intervals/composition.py
+++ intervals/composition.py
@@ -1,4 +1,5 @@
 from intervals.terms import IntegrableFunction
+from intervals.simple_function import SimpleFunction
 from intervals.algebra import Poset
 from numpy import unique, array, fromiter, maximum, minimum, float64
 
@@ -19,6 +20,9 @@ class Composition(IntegrableFunction, Poset):
     def iter_terms(self):
         yield from zip(map(self.coef.__getitem__, self.fmap), self.endpoints)
 
+    def to_simple_function(self):
+        return SimpleFunction(fromiter(map(self.coef.__getitem__, self.fmap), dtype=float64), self.endpoints)
+
     @property
     def endpoints(self):
         return self.sfunc.endpoints
@@ -35,29 +39,29 @@ class Composition(IntegrableFunction, Poset):
         return self.from_coef(self.sfunc, -self.coef)
 
     def __add__(self, other):
-        if isinstance(other, type(self)):
+        if isinstance(other, type(self)) and self.sfunc is other.sfunc:
             return self.from_coef(self.sfunc, self.coef + other.coef)
-        return other + self
+        return self.to_simple_function() + other
 
     def __sub__(self, other):
-        if isinstance(other, type(self)):
+        if isinstance(other, type(self)) and self.sfunc is other.sfunc:
             return self.from_coef(self.sfunc, self.coef - other.coef)
-        return -other + self
+        return self.to_simple_function() - other
 
     def __mul__(self, other):
-        if isinstance(other, type(self)):
+        if isinstance(other, type(self)) and self.sfunc is other.sfunc:
             return self.from_coef(self.sfunc, self.coef * other.coef)
-        return other * self
+        return self.to_simple_function() * other
 
     def __or__(self, other):
-        if isinstance(other, type(self)):
+        if isinstance(other, type(self)) and self.sfunc is other.sfunc:
             return self.from_coef(self.sfunc, maximum(self.coef, other.coef))
-        return other | self
+        return self.to_simple_function() * other
 
     def __and__(self, other):
-        if isinstance(other, type(self)):
+        if isinstance(other, type(self)) and self.sfunc is other.sfunc:
             return self.from_coef(self.sfunc, minimum(self.coef, other.coef))
-        return other & self
+        return self.to_simple_function() & other
 
     def __le__(self, other):
         return self == self & other
